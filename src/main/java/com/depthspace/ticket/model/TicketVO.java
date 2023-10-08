192537679de5a19fa443cb2c466fb514f63989e1
claire <hsyclaire@gmail.com>
2023-10-08 18:25:08 +0800

    update ticketVO

--- src/main/java/com/depthspace/ticket/model/TicketVO.java
+++ src/main/java/com/depthspace/ticket/model/TicketVO.java
@@ -1,7 +1,7 @@
 package com.depthspace.ticket.model;
 
 import java.io.Serializable;
-import java.sql.Date;
+import java.sql.Timestamp;
 
 public class TicketVO implements Serializable{
 
@@ -11,8 +11,9 @@ public class TicketVO implements Serializable{
 	private String description;
 	private Integer price;
 	private Integer stock;
+	private Integer validDays;
 	private byte status;
-	private Date  publishedDate;
+	private Timestamp publishedDate;
 	private Integer totalStarRatings;
 	private Integer totalStars;
 
@@ -23,8 +24,11 @@ public class TicketVO implements Serializable{
 		// TODO Auto-generated constructor stub
 	}
 
+
+
 	public TicketVO(Integer ticketId, Integer ticketTypeId, String ticketName, String description, Integer price,
-			Integer stock,  byte status, Date publishedDate, Integer totalStarRatings, Integer totalStars) {
+			Integer stock, Integer validDays, byte status, Timestamp publishedDate, Integer totalStarRatings,
+			Integer totalStars) {
 		super();
 		this.ticketId = ticketId;
 		this.ticketTypeId = ticketTypeId;
@@ -32,72 +36,145 @@ public class TicketVO implements Serializable{
 		this.description = description;
 		this.price = price;
 		this.stock = stock;
+		this.validDays = validDays;
 		this.status = status;
 		this.publishedDate = publishedDate;
 		this.totalStarRatings = totalStarRatings;
 		this.totalStars = totalStars;
 	}
-	
+
+
+
 	public Integer getTicketId() {
 		return ticketId;
 	}
+
+
+
 	public void setTicketId(Integer ticketId) {
 		this.ticketId = ticketId;
 	}
+
+
+
 	public Integer getTicketTypeId() {
 		return ticketTypeId;
 	}
+
+
+
 	public void setTicketTypeId(Integer ticketTypeId) {
 		this.ticketTypeId = ticketTypeId;
 	}
+
+
+
 	public String getTicketName() {
 		return ticketName;
 	}
+
+
+
 	public void setTicketName(String ticketName) {
 		this.ticketName = ticketName;
 	}
+
+
+
 	public String getDescription() {
 		return description;
 	}
+
+
+
 	public void setDescription(String description) {
 		this.description = description;
 	}
+
+
+
 	public Integer getPrice() {
 		return price;
 	}
+
+
+
 	public void setPrice(Integer price) {
 		this.price = price;
 	}
+
+
+
 	public Integer getStock() {
 		return stock;
 	}
+
+
+
 	public void setStock(Integer stock) {
 		this.stock = stock;
 	}
+
+
+
+	public Integer getValidDays() {
+		return validDays;
+	}
+
+
+
+	public void setValidDays(Integer validDays) {
+		this.validDays = validDays;
+	}
+
+
+
 	public byte getStatus() {
 		return status;
 	}
+
+
+
 	public void setStatus(byte status) {
 		this.status = status;
 	}
-	public Date getPublishedDate() {
+
+
+
+	public Timestamp getPublishedDate() {
 		return publishedDate;
 	}
-	public void setPublishedDate(Date publishedDate) {
+
+
+
+	public void setPublishedDate(Timestamp publishedDate) {
 		this.publishedDate = publishedDate;
 	}
+
+
+
 	public Integer getTotalStarRatings() {
 		return totalStarRatings;
 	}
+
+
+
 	public void setTotalStarRatings(Integer totalStarRatings) {
 		this.totalStarRatings = totalStarRatings;
 	}
+
+
+
 	public Integer getTotalStars() {
 		return totalStars;
 	}
+
+
+
 	public void setTotalStars(Integer totalStars) {
 		this.totalStars = totalStars;
 	}
-	
+
+
 	
 }
