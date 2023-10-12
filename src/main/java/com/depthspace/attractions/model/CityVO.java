80b6761128ad9d5162e2fe3fc0c3c9727a3a11c9
Lisa <lisa860521@gmail.com>
2023-10-12 21:32:06 +0800

    10/12 VO->Hibernate

--- src/main/java/com/depthspace/attractions/model/CityVO.java
+++ src/main/java/com/depthspace/attractions/model/CityVO.java
@@ -2,8 +2,18 @@ package com.depthspace.attractions.model;
 
 import java.io.Serializable;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+@Entity
+@Table(name="CITY")
 public class CityVO implements Serializable{
+	@Id
+	@Column(name="CITY_ID")
 	private Integer cityId;
+	
+	@Column(name="CITY_NAME")
 	private String cityName;
 	public CityVO() {
 		super();
@@ -24,6 +34,10 @@ public class CityVO implements Serializable{
 	}
 	public void setCityName(String cityName) {
 		this.cityName = cityName;
+	}
+	@Override
+	public String toString() {
+		return "CityVO [cityId=" + cityId + ", cityName=" + cityName + "]";
 	}
 	
 }
