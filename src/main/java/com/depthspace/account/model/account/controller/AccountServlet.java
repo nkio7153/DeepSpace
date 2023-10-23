64f2fe9dd8f8d4274fc5187fbb50c4c25f4e88da
Marz23 <s920797252@gmail.com>
2023-10-23 23:33:56 +0800

    10/23 Account表格增刪改查jsp完成

--- src/main/java/com/depthspace/account/model/account/controller/AccountServlet.java
+++ src/main/java/com/depthspace/account/model/account/controller/AccountServlet.java
@@ -48,62 +48,28 @@ public class AccountServlet extends HttpServlet {
 
 	private void listAccounts(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		List<AccountVO> accounts = accountService.getAll();
-//	        req.setAttribute("accounts", accounts);
-//	        req.getRequestDispatcher("/account/list.jsp").forward(req, resp);
 		JSONArray arr = JSONArray.parseArray(JSON.toJSONString(accounts));
 		resp.setContentType("application/json; charset=UTF-8");
 		PrintWriter out = resp.getWriter();
 		out.print(arr.toString());
 	}
-//
-//	private void showAddForm(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-//		req.getRequestDispatcher("/account/add.jsp").forward(req, resp);
-//	}
-//
-//	private void showEditForm(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-//		Integer accountId = Integer.parseInt(req.getParameter("accountId"));
-//		AccountVO account = accountService.findByPrimaryKey(accountId);
-//		req.setAttribute("account", account);
-//		req.getRequestDispatcher("/account/edit.jsp").forward(req, resp);
-//	}
 
 	private void addAccount(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-
 		String accountName = req.getParameter("accountName");
 		Integer memId = Integer.parseInt(req.getParameter("memId"));
-		System.out.println(accountName);
-
 		AccountVO account = new AccountVO(null, accountName, memId);
 		accountService.insert(account);
-
 	}
 
-	private void updateAccount(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-
-//		Integer accountId = Integer.parseInt(req.getParameter("accountId"));
-//		String accountName = req.getParameter("accountName");
-//		Integer memId = Integer.parseInt(req.getParameter("memId"));
-//
-//		AccountVO account = new AccountVO(null, accountName, memId);
-//		accountService.update(account);
-		System.out.println("Update Account");
-		 String accountIdStr = req.getParameter("accountId");
-		    if (accountIdStr != null && !accountIdStr.isEmpty()) {
-		        Integer accountId = Integer.parseInt(accountIdStr);
-		        String accountName = req.getParameter("accountName");
-		        Integer memId = Integer.parseInt(req.getParameter("memId"));
-		        
-		        AccountVO account = new AccountVO(accountId, accountName, memId);
-		        System.out.println(account.toString());
-		        accountService.update(account);
-		    } else {
-		        // 处理无效的 accountId，可能需要返回错误或进行其他适当的操作
-		    }
-
+	private void updateAccount(HttpServletRequest req, HttpServletResponse resp) throws IOException {		
+		Integer accountId = Integer.parseInt(req.getParameter("accountId"));
+		String accountName = req.getParameter("accountName");
+		Integer memId = Integer.parseInt(req.getParameter("memId"));
+		AccountVO account = new AccountVO(accountId, accountName, memId);
+		accountService.update(account);
 	}
 
 	private void deleteAccount(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-		System.out.println(req.getParameter("accountId"));
 		Integer accountId = Integer.parseInt(req.getParameter("accountId"));
 		accountService.delete(accountId);
 	}
--- src/main/webapp/account/add.jsp
+++ src/main/webapp/account/add.jsp
@@ -1,20 +1,67 @@
-
+<%@ page import="com.depthspace.account.model.account.service.AccountServiceImpl" %>
+<%@ page import="com.depthspace.account.model.account.AccountVO" %>
+<%@ page import="java.util.List" %>
+<%@ page import="java.util.HashSet" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
 <!DOCTYPE html>
 <html>
 <head>
-    <meta charset="UTF-8">
-    <title>新增分帳表</title>
+<meta charset="UTF-8">
+<title>新增分帳表</title>
+<script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4" crossorigin="anonymous"></script>
+<script>
+    function processAddSave() {
+        var accountName = document.getElementById("accountName").value;
+        var memId = document.getElementById("memId").value;
+        var accountNameError = document.getElementById("accountNameError");
+        var memIdError = document.getElementById("memIdError");
+        // 清空舊的錯誤訊息
+        accountNameError.textContent = "";
+        memIdError.textContent = "";
+
+        if (accountName.trim() === "") {
+            accountNameError.textContent = '分帳表名稱不能為空';
+            return;
+        }
+
+        if (isNaN(memId) || memId.trim() === "") {
+            memIdError.textContent = '會員ID必須是一個有效的數字';
+            return;
+        }
+
+        $.ajax({
+            type: 'post',
+            data: $('#addForm').serialize(),
+            url: '<%=request.getContextPath()%>/account/account.do?action=add',
+            success: function(data) {
+                alert('新增成功');
+                window.location.href = '<%=request.getContextPath()%>/account/list.jsp';
+            }
+        });
+    }
+</script>
 </head>
 <body>
-    <h1>新增分帳表</h1>
-    <form action="${pageContext.request.contextPath}/account/add.jsp" method="post">
-        <label for="accountName">分帳表名稱:</label>
-        <input type="text" id="accountName" name="accountName" required><br>
-        <label for="memId">會員ID:</label>
-        <input type="number" id="memId" name="memId" required><br>
-        <input type="submit" value="新增分帳表">
+<div id="Add">
+    <form id="addForm" method="Post">
+        <table>
+            <tr>
+                <th>分帳表名稱</th>
+                <td><input type="text" name="accountName" id="accountName">
+                    <span id="accountNameError" style="color: red;"></span>
+                </td>
+            </tr>
+            <tr>
+                <th>會員ID</th>
+                <td><input type="text" name="memId" id="memId">
+                    <span id="memIdError" style="color: red;"></span>
+                </td>
+            </tr>
+        </table>
     </form>
-    <a href="${pageContext.request.contextPath}/account/list.jsp">回到目錄</a>
+</div>
+<input type="button" id="btnUpDateSave" value="儲存" onclick="processAddSave();">
+<input type="button" id="btnCancel" value="取消" onclick="window.location.href='<%=request.getContextPath()%>/account/list.jsp'">
 </body>
 </html>
