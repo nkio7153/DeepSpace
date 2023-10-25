c4042fdb0f437ace610d7f525cbdb3315bfd15d6
Lisa <lisa860521@gmail.com>
2023-10-26 00:35:07 +0800

    1025更新

--- src/main/java/com/depthspace/tour/controller/TourServlet.java
+++ src/main/java/com/depthspace/tour/controller/TourServlet.java
@@ -1,5 +1,56 @@
 package com.depthspace.tour.controller;
 
-public class TourServlet {
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.depthspace.tour.model.tour.TourVO;
+import com.depthspace.tour.service.TourService;
+import com.depthspace.tour.service.TourService_Interface;
+
+@WebServlet({ "/tr/*" })
+public class TourServlet extends HttpServlet {
+//	private static final long serialVersionUID = 1L;
+	private TourService_Interface ts;
 	
+	public void init() throws ServletException{
+		ts = new TourService();
+		System.out.println("成功開啟");
+	}
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		doPost(req, resp);
+	}
+
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		req.setCharacterEncoding("UTF-8");
+		resp.setContentType("text/html;charset=UTF-8");
+		String pathInfo = req.getPathInfo();
+//		System.out.println("pathInfo=" + pathInfo);
+		switch (pathInfo) {
+		case "/tourList":
+			doTourList(req, resp);
+			break;
+		}
+		
+	}
+	//查出該會員所有行程
+	private void doTourList(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		Integer memId;
+//		ts = new TourService();
+		try {
+            memId = Integer.valueOf(req.getParameter("memId"));
+//            System.out.println(memId);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return;
+        }
+		List<TourVO> list = ts.getByMemId(memId);
+		req.setAttribute("list" , list);
+		req.getRequestDispatcher("/tour/index.jsp").forward(req, resp);
+	}
 }
--- src/main/java/com/depthspace/tour/service/TourService.java
+++ src/main/java/com/depthspace/tour/service/TourService.java
@@ -1,5 +1,43 @@
 package com.depthspace.tour.service;
 
-public class TourService {
+import java.util.List;
 
+import com.depthspace.tour.model.tour.TourVO;
+import com.depthspace.tour.model.tour.hibernate.HbTourDAO_Interface;
+
+public class TourService implements TourService_Interface{
+	private HbTourDAO_Interface dao;
+
+	@Override
+	public int insert(TourVO entity) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int update(TourVO entity) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int delete(Integer tourId) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public List<TourVO> getAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<TourVO> getByMemId(Integer memId) {
+		return dao.getByMemId(memId);
+		
+	}
+	
+	
+	
 }
