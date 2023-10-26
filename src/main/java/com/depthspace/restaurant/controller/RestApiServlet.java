3f88932211c958261ee7559652682010007127c1
masa-PC <nkio7153@gmail.com>
2023-10-27 00:18:13 +0800

    add RestCollection API test

--- src/main/java/com/depthspace/restaurant/controller/RestApiServlet.java
+++ src/main/java/com/depthspace/restaurant/controller/RestApiServlet.java
@@ -1,6 +1,7 @@
 package com.depthspace.restaurant.controller;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.List;
 
 import javax.servlet.ServletException;
@@ -12,10 +13,12 @@ import javax.servlet.http.HttpServletResponse;
 import com.depthspace.restaurant.model.restcollection.RestCollectionVO;
 import com.depthspace.restaurant.service.RestcollectionService;
 import com.depthspace.restaurant.service.RestcollectionServiceImpl;
+import com.google.gson.Gson;
 
 @WebServlet("/RestApi/*")
 public class RestApiServlet extends HttpServlet {
 	
+	private Gson gson;
 	private RestcollectionService restcollectionService;
 	
 	@Override
@@ -25,7 +28,7 @@ public class RestApiServlet extends HttpServlet {
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		super.doGet(req, resp);
+		doPost(req, resp);
 	}
 
 	@Override
@@ -37,16 +40,54 @@ public class RestApiServlet extends HttpServlet {
 			case "/getRestCollectionAll":
 				getRestCollectionAll(req, resp);
 				break;
+			case "/RestCollection":
+				doRestCollection(req, resp);
+				break;
+				
 		}
 		
 	}
 	
 	private void getRestCollectionAll(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		List<RestCollectionVO> rcList = restcollectionService.getAll();
-		req.setAttribute("rcList", rcList);
-		req.getRequestDispatcher("Rest/ShowList.jsp").forward(req, resp);
+//		JSONArray arr = JSONArray.parseArray(JSON.toJSONString(rcList));		
+//		resp.setContentType("application/json; charset=UTF-8");
+//		PrintWriter out = resp.getWriter();
+//		out.print(arr.toString());
 		
 	}
+	
+	private void doRestCollection(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		PrintWriter out = resp.getWriter();
+		String action = req.getParameter("action");
+		if (action.equals("add")) {
+			try {
+				RestCollectionVO vo = new RestCollectionVO();
+				vo.setMemId(Integer.valueOf(req.getParameter("memId")));
+				vo.setRestId(Integer.valueOf(req.getParameter("restId")));
+				restcollectionService.add(vo);
+				out.print("SUCCESS");
+			} catch (Exception e) {
+				out.print("ERROR");
+				e.printStackTrace();
+			}
+		} else if (action.equals("delete")) {
+			try {
+				RestCollectionVO vo = new RestCollectionVO();
+				vo.setMemId(Integer.valueOf(req.getParameter("memId")));
+				vo.setRestId(Integer.valueOf(req.getParameter("restId")));
+				restcollectionService.delete(vo);
+				out.print("SUCCESS");
+			} catch (Exception e) {
+				out.print("ERROR");
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	
+	
+	
 	
 	
 	
