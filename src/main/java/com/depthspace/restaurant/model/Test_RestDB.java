2d73879c6b1250f4579435c7cfc1ced41800d773
masa <nkio7153@gmail.com>
2023-10-30 15:16:50 +0800

    chage testfile

--- src/main/java/com/depthspace/restaurant/model/Test_RestDB.java
+++ src/main/java/com/depthspace/restaurant/model/Test_RestDB.java
@@ -12,13 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.hibernate.Session;
 
-import com.depthspace.restaurant.model.restaurant.RestDAO;
-import com.depthspace.restaurant.model.restaurant.RestDAOHibernateImpl;
 import com.depthspace.restaurant.model.restaurant.RestVO;
-import com.depthspace.restaurant.model.restaurant.DAO.RestDAO_interface;
-import com.depthspace.restaurant.model.restaurant.DAO.RestHibernateDAO;
-import com.depthspace.restaurant.model.restaurant.DAO.RestJDBCDAOImpl;
-import com.depthspace.restaurant.model.restbookingdate.RestBookingDateHibernateDAOImpl;
 import com.depthspace.utils.HibernateUtil;
 
 @WebServlet("/Test_RestDB")
