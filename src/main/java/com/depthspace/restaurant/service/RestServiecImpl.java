0ab1dc130fc729d3c7ed3bce4e428fd5816fd63d
masa <nkio7153@gmail.com>
2023-10-30 15:23:24 +0800

    file test

--- src/main/java/com/depthspace/restaurant/service/RestServiecImpl.java
+++ src/main/java/com/depthspace/restaurant/service/RestServiecImpl.java
@@ -2,10 +2,7 @@ package com.depthspace.restaurant.service;
 
 import java.util.List;
 
-import org.hibernate.Session;
-
 import com.depthspace.restaurant.model.restaurant.RestDAO;
-import com.depthspace.restaurant.model.restaurant.RestDAOHibernateImpl;
 import com.depthspace.restaurant.model.restaurant.RestDAOImpl;
 import com.depthspace.restaurant.model.restaurant.RestVO;
 import com.depthspace.utils.HibernateUtil;
@@ -15,7 +12,6 @@ public class RestServiecImpl implements RestService {
 	private RestDAO dao;
 	
 	public RestServiecImpl() {
-//		dao = new RestDAOHibernateImpl();
 		dao = new RestDAOImpl(HibernateUtil.getSessionFactory());
 	}
 	
