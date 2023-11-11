872f10029a4a3b492a7b16b3dcbabd67189ab748
wang <pnn0908128305@gmail.com>
2023-11-11 11:46:38 +0800

    更

--- src/main/java/com/depthspace/admin/dto/QueryAdminParameter.java
+++ src/main/java/com/depthspace/admin/dto/QueryAdminParameter.java
@@ -1,11 +1,11 @@
-package com.depthspace.admin.dto;
-
-import lombok.Data;
-import org.springframework.data.relational.core.sql.In;
-
-@Data
-public class QueryAdminParameter {
-    private  String search;
-    private Integer page;//當前頁數
-    private Integer size;//該頁顯示筆數
-}
+//package com.depthspace.admin.dto;
+//
+//import lombok.Data;
+//import org.springframework.data.relational.core.sql.In;
+//
+//@Data
+//public class QueryAdminParameter {
+//    private  String search;
+//    private Integer page;//當前頁數
+//    private Integer size;//該頁顯示筆數
+//}
