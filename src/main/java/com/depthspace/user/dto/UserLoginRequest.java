b4862d0b2c3f694d190e88f42d21373a06a6ffa9
wang <pnn0908128305@gmail.com>
2023-11-11 16:11:52 +0800

    推

--- src/main/java/com/depthspace/user/dto/UserLoginRequest.java
+++ src/main/java/com/depthspace/user/dto/UserLoginRequest.java
@@ -1,29 +1,29 @@
-//package com.depthspace.user.dto;
-//
-//import javax.validation.constraints.Email;
-//import javax.validation.constraints.NotBlank;
-//
-//public class UserLoginRequest {
-//
-//    @NotBlank
-//    @Email
-//    private String Email;
-//    @NotBlank
-//    private String password;
-//
-//    public String getEmail() {
-//        return Email;
-//    }
-//
-//    public void setEmail(String email) {
-//        Email = email;
-//    }
-//
-//    public String getPassword() {
-//        return password;
-//    }
-//
-//    public void setPassword(String password) {
-//        this.password = password;
-//    }
-//}
+package com.depthspace.user.dto;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
+public class UserLoginRequest {
+
+    @NotBlank
+    @Email
+    private String Email;
+    @NotBlank
+    private String password;
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String email) {
+        Email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
